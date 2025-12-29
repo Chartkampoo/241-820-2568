841b62fa68ec3b7f2a1a9c8923f39ecd91a96c2b
Chartkampoo <chartkampoo.s@ki.th>
2025-12-29 16:59:09 +0700

    add

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+/** 
+loop statements:
+while
+for
+*/
+let counter = 0;
+while(counter<10){
+    console.log('Hello World');
+    //counter = counter + 1;
+    counter += 1;
+    counter ++;
+}
+for(let i=0; i<10; i++){
+    console.log('Hello World from for loop');
+}
+
+
