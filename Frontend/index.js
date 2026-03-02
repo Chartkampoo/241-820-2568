7e2f5bbc2658b47bff3a689bd729c4fe00b116cb
Chartkampoo <chartkampoo.s@ku.th>
2026-03-02 17:45:19 +0700

    Add files via upload

---
+++ Frontend/index.js
@@ -0,0 +1,40 @@
+async function submitData() {
+    let firstNameDOM = document.querySelector('input[name=firstname]');
+    let lastNameDOM = document.querySelector('input[name=lastname]');
+    let ageDOM = document.querySelector('input[name=age]');
+    let genderDOM = document.querySelector('input[name=gender]:checked');
+    let interestDOMs = document.querySelectorAll('input[name=interests]:checked');
+    let descriptionDOM = document.querySelector('textarea[name=description]');
+
+    let messageDOM = document.getElementById('message')
+    try {
+    let interest = '';
+    for (let i = 0; i < interestDOMs.length; i++) {
+        interest += interestDOMs[i].value;
+        if (i != interestDOMs.length - 1) {
+            interest += ',';
+        }
+    }
+    let userData = {
+        firstname: firstNameDOM.value,
+        lastname: lastNameDOM.value,
+        age: ageDOM.value,
+        gender: genderDOM.value,
+        description: descriptionDOM.value,
+        interests: interest
+    }
+        const response = await axios.post('http://localhost:8000/users',userData)
+        console.log('response', response.data);
+        messageDOM.innerText = 'บันทึกข้อมูลสำเร็จ'
+        messageDOM.className = 'message success'
+    } catch (error) {
+        if (error.response) {
+            console.error('Error creating user:', error.response.data.messages);
+        }
+        messageDOM.innerText = 'เกิดข้อผิดพลาดในการบันทึกข้อมูล'
+        messageDOM.className = 'message danger'
+    }
+
+    
+
+}
